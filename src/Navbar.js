c624ca158f9a0d3a996735203c690220689efaef
curran <curran.kelleher@gmail.com>
2019-01-21 14:49:15 +0530

    Add Bulma Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,9 @@ import React from 'react';
 export const Navbar = () => (
   <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
-      <a className="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-      </a>
+      <div className="navbar-item">
+        VizHub
+      </div>
 
       <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
