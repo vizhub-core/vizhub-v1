223db90ec5a96e8c12a2fd5e371893729f83e03e
curran <curran.kelleher@gmail.com>
2019-02-02 22:49:29 +0530

    Establish studio structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { LoadingScreen } from './components/LoadingScreen';
+import { Studio } from './components/Studio';
 
-export const App = () => (
-  <LoadingScreen/>
-)
+export const App = () => {
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 2000);
+  }, []);
+
+  return loaded ? <Studio/> : <LoadingScreen/>;
+};
--- src/components/Studio.js
+++ src/components/Studio.js
@@ -1,2 +1,31 @@
-export const Studio = () => (
-);
+import React from 'react';
+import styled from 'styled-components';
+
+const StudioContainer = styled.div`
+  display: flex;
+`;
+
+const ConfiguratorContainer = styled.div`
+  flex-grow: 1;
+`;
+
+const EditorContainer = styled.div`
+  flex-grow: 4;
+`;
+
+const ViewerContainer = styled.div`
+  flex-grow: 2;
+`;
+
+export const Studio = () =>
+  <StudioContainer>
+    <ConfiguratorContainer>
+      Configurator
+    </ConfiguratorContainer>
+    <EditorContainer>
+      Editor
+    </EditorContainer>
+    <ViewerContainer>
+      Viewer
+    </ViewerContainer>
+  </StudioContainer>
