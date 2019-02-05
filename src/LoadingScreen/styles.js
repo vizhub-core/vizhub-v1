ea107ba8bebc83693460093ee98bcb19d85c1265
curran <curran.kelleher@gmail.com>
2019-02-05 16:53:58 +0530

    Refactor loading screen

--- src/LoadingScreen/styles.js
+++ src/LoadingScreen/styles.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import logo from './svg/logo.svg';
 
-const SpinningLogoWrapper = styled.div`
+export const LoadingScreenWrapper = styled.div`
   background-color: #4567aa;
-  min-height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -17,15 +16,7 @@ const spin = keyframes`
   to { transform: rotate(360deg); }
 `;
 
-const SpinningImage = styled.img`
+export const SpinningImage = styled.img`
   animation: ${spin} infinite 50s linear;
   height: 25vmin;
 `;
-
-const SpinningLogo = () => <SpinningImage src={logo} />;
-
-export const LoadingScreen = () => (
-  <SpinningLogoWrapper>
-    <SpinningLogo />
-  </SpinningLogoWrapper>
-);
