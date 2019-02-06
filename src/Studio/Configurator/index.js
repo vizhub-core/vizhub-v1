2658e36254ace3159210a8942d7b421a977ba59d
curran <curran.kelleher@gmail.com>
2019-02-06 10:59:19 +0530

    Create Configurator Sections

---
+++ src/Studio/Configurator/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Section } from './styles';
+
+export const Configurator = () => (
+  <>
+    <Section>Design</Section>
+    <Section>Code</Section>
+    <Section>Dependencies</Section>
+    <Section>Settings</Section>
+  </>
+);
---
+++ src/Studio/Configurator/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Section = styled.div`
+  border: 1px solid ${props => props.theme.foreground}
+  padding: 10px;
+`;
