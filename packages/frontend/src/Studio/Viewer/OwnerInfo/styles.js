3acf3a45825aecd00fea40fdfa4143671ebb9695
curran <curran.kelleher@gmail.com>
2019-02-23 22:54:31 +0530

    Refactor: introduce Flex component to reduce duplication

--- packages/frontend/src/Studio/Viewer/OwnerInfo/styles.js
+++ packages/frontend/src/Studio/Viewer/OwnerInfo/styles.js
@@ -1,18 +1,17 @@
 import styled from 'styled-components';
 import { Avatar } from '../styles';
+import { Flex } from '../../../styles';
 
 export const InfoAvatar = styled(Avatar)`
   height: ${props => props.theme.infoAvatarHeight}px;
 `;
 
-export const Wrapper = styled.div`
-  display: flex;
+export const Wrapper = styled(Flex)`
   font-size: 1.1em;
 `;
 
-export const OwnerNamePublishDate = styled.div`
+export const OwnerNamePublishDate = styled(Flex)`
   margin-left: ${props => props.theme.infoAvatarPadding}px;
-  display: flex;
   flex-direction: column;
   justify-content: center;
 `;
